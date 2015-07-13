fa217dd99e040c6dc2901c38a3dce99947745c78
Jeremy Warner <jeremywrnr@gmail.com>
2015-07-13 17:17:27 -0400

    better button routing

--- app/client/javascript/client.js
+++ app/client/javascript/client.js
@@ -6,3 +6,28 @@ Meteor.subscribe('messages');
 Session.setDefault("document", null);
 Session.setDefault("renaming", false);
 Session.setDefault("editorType", "ace");
+
+Template.navigation.helpers({
+
+  navItems: function(){
+    return [
+      {
+        iconpath:'/',
+        iconname:'pencil',
+      },
+      {
+        iconpath:'/tests',
+        iconname:'play'
+      },
+      {
+        iconpath:'/git',
+        iconname:'list-alt'
+      },
+      {
+        iconpath:'/settings',
+        iconname:'cog'
+      }
+    ]
+  }
+
+});
