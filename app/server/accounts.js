fd66e38a89158292e3b2bee19f336bd31c422dec
Jeremy Warner <jeremywrnr@gmail.com>
2015-07-15 19:47:08 -0400

    make accounts

---
+++ app/server/accounts.js
@@ -0,0 +1,14 @@
+Accounts.onCreateUser(function(options, user){
+
+  var accessToken = user.services.github.accessToken, result, profile;
+  result = Meteor.http.get("https://api.github.com/user", {
+    headers: { "User-Agent": "Code Pilot" },
+    params: { access_token: accessToken }
+  });
+  if (result.error) throw result.error;
+  profile = _.pick( result.data,
+                   "login", "name", "avatar_url", "url", "blog", "email", "html_url");
+  user.profile = profile;
+  return user;
+
+})
