793e10e1af5faa353ed325f81d5fcf6f125f7cde
Jeremy Warner <jeremywrnr@gmail.com>
2015-07-16 13:36:11 -0400

    add pilot and copilot methods

--- app/lib/common.js
+++ app/lib/common.js
@@ -3,4 +3,5 @@
 GithubInfo = new Mongo.Collection('githubInfo')
 Messages = new Mongo.Collection('messages');
 Files = new Mongo.Collection('files');
-Tasks = new Mongo.Collection("tasks");
+Users = new Mongo.Collection('users');
+Tasks = new Mongo.Collection('tasks');
--- app/server/methods.js
+++ app/server/methods.js
@@ -1,9 +1,19 @@
+// server methods
+
 (function () {
   Meteor.methods({
 
     deleteFile: function(id) {
       Files.remove(id);
       ShareJS.model["delete"](id);
+    },
+
+    setPilot: function(id) {
+      return Users.update({"_id":id, {$set : {"role":"pilot"}} });
+    },
+
+    setCopilot: function(id) {
+      return Users.update({"_id":id, {$set : {"role":"copilot"}} });
     }
 
   });
