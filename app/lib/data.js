0d9ca30bfe4e2677854899d0e2bdd598a2d6878a
Jeremy Warner <jeremywrnr@gmail.com>
2015-07-22 11:37:33 -0400

    remove unused collection

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,6 +1,5 @@
 // data publishing
 
-GithubInfo = new Mongo.Collection('githubInfo')
 Messages = new Mongo.Collection('messages');
 Files = new Mongo.Collection('files');
 Tasks = new Mongo.Collection('tasks');
