51b5d608117830e419e588edd2954a4b75af18fd
Jeremy Warner <jeremywrnr@gmail.com>
2015-08-02 17:54:10 -0700

    re-require login

--- app/lib/routing.js
+++ app/lib/routing.js
@@ -3,13 +3,11 @@
 Router.configure({ layoutTemplate: 'main' });
 
 // asks user to login before coding
-/*
- *Router.onBeforeAction(function() {
- *  if (! Meteor.userId()) {
- *    this.render('config');
- *  } else { this.next(); }
- *});
- */
+Router.onBeforeAction(function() {
+  if (! Meteor.userId()) {
+    this.render('config');
+  } else { this.next(); }
+});
 
 Router.map(function () {
   this.route('code', { path: '/', });
