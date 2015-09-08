6660e7dd1b7d8223903d4b596df6f289b6382614
Jeremy Warner <jeremywrnr@gmail.com>
2015-09-07 20:36:28 -0400

    trying to render div

--- app/client/js/git.js
+++ app/client/js/git.js
@@ -5,6 +5,33 @@ var ufiles = Meteor.g.userfiles;
 var focusForm = Meteor.g.focusForm;
 var labelLineNumbers = Meteor.g.labelLineNumbers;
 
+// do a javascript diff
+var diffUsingJS = function(file) {
+  if (file.content === file.cache) return; // nodiff
+  var byId = function (id) { return document.getElementById(id); },
+    base = difflib.stringAsLines( file.cache ),
+    newtxt = difflib.stringAsLines( file.content ),
+    sm = new difflib.SequenceMatcher(base, newtxt),
+    opcodes = sm.get_opcodes(),
+    diffoutputdiv = byId(file._id),
+    contextSize = 10;
+
+  diffoutputdiv.innerHTML = "";
+
+  diffoutputdiv.appendChild(diffview.buildView({
+    baseTextLines: base,
+    newTextLines: newtxt,
+    opcodes: opcodes,
+    baseTextName: "base",
+    newTextName: "new",
+    contextSize: contextSize,
+    // 0 for side by side
+    // 1 for inline diff
+    viewType: 0,
+  }));
+};
+
+
 Template.commitPanel.helpers({
 
   branch: function() {
@@ -16,16 +43,15 @@ Template.commitPanel.helpers({
   },
 
   diffs: function() { // using jsdiff, return a diff on each file
-    ufiles().map(function checkDiff(file){ // compare content + cache
-      if(file.content !== file.cache)
-        return { // return a named diff
+    return ufiles().map(function checkDiff(file){ // content v cache
+      if(file.content !== file.cache) // return the different file
+        return {
           id: file._id,
           title: file.title,
-          diff: labelLineNumbers(
-            diffString(file.cache, file.content)
-          ).replace(/  /g, ' ')
+          content: file.content,
+          cache: file.cache
         };
-    }).filter(function removeNull(diff){ // remove unchanged files from array
+    }).filter(function removeNull(diff){
       return diff != undefined;
     });
   },
@@ -104,6 +130,38 @@ Template.commit.events({
     var trulyLoad = confirm("This will overwrite any uncommited changes. Proceed?");
     if (trulyLoad)
       Meteor.call('loadCommit', this.commit.sha);
+  },
+
+});
+
+Template.diff.helpers({
+
+  renderDiff: function() {
+    if (this.content === this.cache)
+      return; // nodiff
+
+    //var byid = function (id) { return this.$('#'+id); },
+      //base = difflib.stringaslines( this.cache ),
+      //newtxt = difflib.stringaslines( this.content ),
+      //sm = new difflib.sequencematcher(base, newtxt),
+      //opcodes = sm.get_opcodes(),
+      //diffoutputdiv = byid(this.id),
+      //contextsize = 10;
+
+    //console.log(diffoutputdiv);
+    //diffoutputdiv.innerhtml = "";
+
+    //diffoutputdiv[0].appendchild(diffview.buildview({
+      //basetextlines: base,
+      //newtextlines: newtxt,
+      //opcodes: opcodes,
+      //basetextname: "base",
+      //newtextname: "new",
+      //contextsize: contextsize,
+      //// 0 for side by side
+      //// 1 for inline diff
+      //viewtype: 0,
+    //}));
   },
 
 });
