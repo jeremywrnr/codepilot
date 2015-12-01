14c952f67de1fc170120ed02c3b07481394d3455
Jeremy Warner <jeremywrnr@gmail.com>
2015-12-01 12:20:50 -0500

    adding all types of tests

---
+++ app/tests/jasmine/server/integration/testSpec.js
@@ -0,0 +1,7 @@
+describe("destroying all dreams", function () {
+  //beforeEach(function (done) { selectGraceHopper(done); });
+
+  it("should pass no test apparently", function () {
+    expect(1).toEqual(1);
+  });
+});
