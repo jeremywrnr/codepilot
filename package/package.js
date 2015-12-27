1662c84529e909af5b20a5cf89781a14f644cb7a
Jeremy Warner <jeremywrnr@gmail.com>
2015-12-27 01:36:09 -0500

    merge all into test

---
+++ package/package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: "jeremywrnr:codepilot",
+  summary: "Pair programming.",
+  version: "0.1",
+  git: "https://github.com/jeremywrnr/codepilot"
+});
+
+Package.onUse(function(api) {
+  api.export('MeteorCamera');
+  api.versionsFrom("METEOR@1.2");
+  api.use("isobuild:cordova@5.2.0");
+
+  api.addFiles('photo.html');
+  api.addFiles('photo.js');
+  api.addFiles("camera.less", ["web.browser"]);
+  api.addFiles('photo-browser.js', ['web.browser']);
+  api.addFiles('photo-cordova.js', ['web.cordova']);
+});
