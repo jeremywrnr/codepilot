823cd15a2dd48b9798351e23296c7c0d0aade48e
Jeremy Warner <jeremywrnr@gmail.com>
2016-01-05 08:46:13 +0800

    adding linkify tests

--- packages/codepilot/codepilot-tests.js
+++ packages/codepilot/codepilot-tests.js
@@ -1,6 +1,16 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+// linkify tests
+
+Tinytest.add('linkify', function (test) {
+  var link = function(x) { '<a target="_blank" href="' + s + '">' + s + '</a>' }
+  test.equal(true, true)
+
+  var x = Codepilot.linkify('x')
+  test.equal(x, ['x'])
+
+  var y = Codepilot.linkify('y.com')
+  test.equal(y, [link('y.com')])
+
+  var z = Codepilot.linkify('1', 'hi.net')
+  test.equal(z, ['1', link('hi.net')])
 });
 
