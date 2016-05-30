c159008fb948b44cfdc0f426b8c9c39cbf9cb18f
Jeremy Warner <jeremywrnr@gmail.com>
2016-05-30 15:48:07 -0400

    whitelist password

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,7 @@
 # bring config vars into environment
 export_env_dir() {
   env_dir=$3
-  whitelist_regex=${2:-''}
+  whitelist_regex=${2:-'GH_PASSWORD'}
   blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
