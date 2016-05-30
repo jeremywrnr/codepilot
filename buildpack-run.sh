42679fae125f1d91bd8f487014812048c192710c
Jeremy Warner <jeremywrnr@gmail.com>
2016-05-30 15:59:33 -0400

    only blacklist

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,11 +1,10 @@
 # bring config vars into environment
 export_env_dir() {
   env_dir=$3
-  whitelist_regex=${2:-'GH_PASSWORD'}
-  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  blacklist=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
-      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      echo "$e" | grep -qvE "$blacklist_regex" &&
       export "$e=$(cat $env_dir/$e)"
       :
     done
