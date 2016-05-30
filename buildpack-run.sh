22bdb78b80f47d639231f48e2d2f3d1f558e90cc
Jeremy Warner <jeremywrnr@gmail.com>
2016-05-30 15:44:51 -0400

    fix heroku config:

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,1 +1,16 @@
-cd private && make decrypt
+# bring config vars into environment
+export_env_dir() {
+  env_dir=$3
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+
+# decrypting API keys
+cd private/ && make decrypt
